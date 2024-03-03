73ca972097d01b9616bbafbef9e3444824b67474
Nazmul <mdnazmulhasan0879@gmail.com>
2024-03-03 15:40:15 +0600

    some modification

--- Script.js
+++ Script.js
@@ -1,15 +1,19 @@
 let navmanu = document.querySelector(".nav-list");
 let togglebtn = document.querySelector(".toggle-btn");
-let mybars = document.querySelector(".bar")
+let mybars = document.querySelector(".bar");
+let toggleSubmanu = document.querySelector("#sublist-manu-1");
+let submanu = document.querySelector(".sub-list");
  
 togglebtn.addEventListener("click", () => {
         togglebtn.classList.toggle("icon");
         navmanu.classList.toggle("open");
     });
-
+toggleSubmanu.addEventListener("click", ()=>{
+   submanu.classList.toggle("openSubmanu-all");
+});
 document.onclick = function(e){
     if(!togglebtn.contains(e.target)&& !navmanu.contains(e.target)){
-        togglebtn.classList.toggle("icon");
-      navmanu.classList.remove("open");
+        togglebtn.classList.remove("icon");
+        navmanu.classList.remove("open");
     }
-}
+};
